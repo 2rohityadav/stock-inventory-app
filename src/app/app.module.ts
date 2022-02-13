e26a8149d257a974b65f283c4ccdb309a6f38980
Rohit Yadav <rohityadav.techie@gmail.com>
2022-02-13 05:40:51 -0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { StockInventoryModule } from './stock-inventory/stock-inventory.module';
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, StockInventoryModule],
   providers: [],
   bootstrap: [AppComponent]
 })
